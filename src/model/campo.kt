1bd52b640c0eb634a2bbdcecb4c08ffbef187c65
Leonardo Lemes Bufalo <leonardolsbufalo@gmail.com>
2019-03-13 17:29:47 -0300

    final Amazing Commit

--- src/model/campo.kt
+++ src/model/campo.kt
@@ -1,8 +1,9 @@
-package model
+package modelo
 
-enum class CampoEvento { ABERTURA, MARCADO, DESMARCADO, EXPLOSAO, REINICIALIZACAO, MACACAO, DESMARCACAO }
+enum class CampoEvento { ABERTURA, MARCACAO, DESMARCACAO, EXPLOSAO, REINICIALIZACAO }
 
 data class Campo(val linha: Int, val coluna: Int) {
+
     private val vizinhos = ArrayList<Campo>()
     private val callbacks = ArrayList<(Campo, CampoEvento) -> Unit>()
 
@@ -10,27 +11,28 @@ data class Campo(val linha: Int, val coluna: Int) {
     var aberto: Boolean = false
     var minado: Boolean = false
 
-    // somente leitura
+    // Somente leitura
     val desmarcado: Boolean get() = !marcado
     val fechado: Boolean get() = !aberto
     val seguro: Boolean get() = !minado
     val objetivoAlcancado: Boolean get() = seguro && aberto || minado && marcado
-    val qtdVizinhosMinados: Int get() = vizinhos.filter { it.minado }.size
-    val vizinhancaSegura: Boolean get() = vizinhos.map { it.seguro }.reduce { resultado, seguro -> resultado && seguro }
+    val qtdeVizinhosMinados: Int get() = vizinhos.filter { it.minado }.size
+    val vizinhancaSegura: Boolean
+        get() = vizinhos.map { it.seguro }.reduce { resultado, seguro -> resultado && seguro }
 
-    fun addVizinhos(vizinho: Campo) {
+    fun addVizinho(vizinho: Campo) {
         vizinhos.add(vizinho)
     }
 
-    fun onEvento(callback: (Campo, CampoEvento) -> Unit ) {
+    fun onEvento(callback: (Campo, CampoEvento) -> Unit) {
         callbacks.add(callback)
     }
 
     fun abrir() {
-        if( fechado ) {
+        if (fechado) {
             aberto = true
-            if( minado ){
-                callbacks.forEach { it(this, CampoEvento.EXPLOSAO  ) }
+            if (minado) {
+                callbacks.forEach { it(this, CampoEvento.EXPLOSAO) }
             } else {
                 callbacks.forEach { it(this, CampoEvento.ABERTURA) }
                 vizinhos.filter { it.fechado && it.seguro && vizinhancaSegura }.forEach { it.abrir() }
@@ -41,7 +43,7 @@ data class Campo(val linha: Int, val coluna: Int) {
     fun alterarMarcacao() {
         if (fechado) {
             marcado = !marcado
-            val evento = if(marcado) CampoEvento.MACACAO else CampoEvento.DESMARCACAO
+            val evento = if(marcado) CampoEvento.MARCACAO else CampoEvento.DESMARCACAO
             callbacks.forEach { it(this, evento) }
         }
     }
@@ -50,11 +52,10 @@ data class Campo(val linha: Int, val coluna: Int) {
         minado = true
     }
 
-    fun reiniciar(){
+    fun reiniciar() {
         aberto = false
         minado = false
         marcado = false
         callbacks.forEach { it(this, CampoEvento.REINICIALIZACAO) }
     }
-
 }
